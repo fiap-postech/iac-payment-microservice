647e4fef70a7f9e71bdf66297e4635e35a1f91dc
Eberson Oliveira <eberson.olivera@gmail.com>
2024-03-10 19:11:02 -0300

    fix: payment confirm should not be authorized for now

--- api_gateway.tf
+++ api_gateway.tf
@@ -34,10 +34,10 @@ resource "aws_apigatewayv2_route" "apigw_route_to_root" {
   ]
 }
 
-resource "aws_apigatewayv2_route" "apigw_route_to_root" {
-  api_id             = data.aws_apigatewayv2_api.tech_challenge_api.id
-  route_key          = "ANY /${lower(local.context_name)}/confirm"
-  target             = "integrations/${aws_apigatewayv2_integration.apigw_integration.id}"
+resource "aws_apigatewayv2_route" "apigw_route_to_confirm" {
+  api_id    = data.aws_apigatewayv2_api.tech_challenge_api.id
+  route_key = "ANY /${lower(local.context_name)}/confirm"
+  target    = "integrations/${aws_apigatewayv2_integration.apigw_integration.id}"
   depends_on = [
     aws_apigatewayv2_integration.apigw_integration
   ]
